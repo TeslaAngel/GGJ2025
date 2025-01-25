d3383dd6cc0161ea90d87852c7eac36f69f571cf
TeslaAngel <mohadot@outlook.com>
2025-01-25 14:21:50 -0800

    deck system and card raycast - pending engine management

--- Assets/Scripts/DeckSystem.cs
+++ Assets/Scripts/DeckSystem.cs
@@ -49,8 +49,23 @@ public class DeckSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //remove any null elements, then move the other cards in place
+        for (int i = 0; i < cardsInDeck.Count; i++)
+        {
+            if (cardsInDeck[i] == null)
+            {
+                cardsInDeck.RemoveAt(i);
+
+                //reposition the rest of the cards
+                for (int j = 0; j < cardsInDeck.Count; j++)
+                {
+                    cardsInDeck[j].transform.position = cardSlotStartingPosition + j * cardSlotSpaceInterval;
+                }
+            }
+        }
+
         //when timer zeros, give a new card
-        if(rollCardTimer <= 0 && cardsInDeck.Count< maxCardCount)
+        if (rollCardTimer <= 0 && cardsInDeck.Count< maxCardCount)
         {
             //Instantiate the card in scene
             cardsInDeck.Add(Instantiate(selectRandomCard(), cardSlotStartingPosition + cardsInDeck.Count * cardSlotSpaceInterval, Quaternion.identity, transform));
