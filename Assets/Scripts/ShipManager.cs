20164a592ca435df146e31bb13f5ab087d6a57ee
TeslaAngel <mohadot@outlook.com>
2025-01-26 15:17:11 -0800

    working mobile terrain system

--- Assets/Scripts/ShipManager.cs
+++ Assets/Scripts/ShipManager.cs
@@ -14,7 +14,9 @@ public class ShipManager : MonoBehaviour
     [Space]
     public float turningRotationAngle;
     public float turningSpeed;
+    private float turningSpeedFactor = 1f;
     public float switchLaneSpeed;
+    private float switchLaneSpeedFactor = 1f;
 
     [Space]
     public Vector3[] lanePositions;
@@ -43,7 +45,7 @@ public class ShipManager : MonoBehaviour
         if (Input.GetAxis("Vertical") > 0.3f)
         {
             //gradually rotate to the left
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, 0f, turningRotationAngle), turningSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, 0f, turningRotationAngle), turningSpeed * turningSpeedFactor * Time.deltaTime);
 
             //switch to left (upper) lane
             if (Input.GetAxis("Vertical") > 0.5f && !switchedLane && currentLane>0)
@@ -55,7 +57,7 @@ public class ShipManager : MonoBehaviour
         else if (Input.GetAxis("Vertical") < -0.3f)
         {
             //gradually rotate to the right
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, 0f, -turningRotationAngle), turningSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, 0f, -turningRotationAngle), turningSpeed * turningSpeedFactor * Time.deltaTime);
 
             //switch to right (lower) lane
             if (Input.GetAxis("Vertical") < -0.5f && !switchedLane && currentLane < lanePositions.Length-1)
@@ -69,7 +71,7 @@ public class ShipManager : MonoBehaviour
             //back to original rotation
             if (Quaternion.Angle(transform.rotation, Quaternion.identity) > 2f)
             {
-                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.identity, turningSpeed * Time.deltaTime);
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.identity, turningSpeed * turningSpeedFactor * Time.deltaTime);
             }
 
             switchedLane = false;
@@ -79,9 +81,12 @@ public class ShipManager : MonoBehaviour
         //always align to lane position
         if(lanePositions.Length > 0)
         {
-            transform.position = Vector3.Lerp(transform.position, lanePositions[currentLane], switchLaneSpeed * Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, lanePositions[currentLane], switchLaneSpeed * switchLaneSpeedFactor * Time.deltaTime);
         }
 
+
+        turningSpeedFactor = 1f;
+        switchLaneSpeedFactor = 1f;
 
         //impose effect by type of bubbles in each engine
         foreach (EngineCode code in EngineList)
@@ -91,19 +96,22 @@ public class ShipManager : MonoBehaviour
                 switch (code.bubble.GetComponent<BubbleCode>().bubbleType)
                 {
                     case 1: //light bubble
-                        //GetComponent<Rigidbody>().AddForce(Vector3.up * lightBubbleForce * Time.deltaTime);
+                        //nothing For now
                         break;
 
                     case 2: //heavy bubble
-                        //GetComponent<Rigidbody>().AddForce(Vector3.down * heavyBubbleForce * Time.deltaTime);
+                        turningSpeedFactor *= 0.5f;
+                        switchLaneSpeedFactor *= 0.5f;
                         break;
 
                     case 3: //speed bubble
-                        //GetComponent<Rigidbody>().AddForce(Vector3.forward * fastBubbleForce * Time.deltaTime);
+                        turningSpeedFactor *= 2f;
+                        switchLaneSpeedFactor *= 2f;
                         break;
 
                     case 4: //slow bubble
-                        //GetComponent<Rigidbody>().AddForce(Vector3.back * slowBubbleForce * Time.deltaTime);
+                        turningSpeedFactor *= 0.5f;
+                        switchLaneSpeedFactor *= 0.5f;
                         break;
                 }
             }
