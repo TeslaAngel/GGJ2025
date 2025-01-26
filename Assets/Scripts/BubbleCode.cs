a3a3ca3c6fe363c5a35b73602bcdd80e01f8312e
TeslaAngel <mohadot@outlook.com>
2025-01-25 19:54:53 -0800

    renew level1

--- Assets/Scripts/BubbleCode.cs
+++ Assets/Scripts/BubbleCode.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BubbleCode : MonoBehaviour
 {
+    public int bubbleType; //0 = sample, 1 = light, 2 = heavy, 3 = speed, 4 = slow, 5 = TNT
+
     private void OnCollisionEnter(Collision collision)
     {
         //destroy the bubble when being hit by spiky enemy
