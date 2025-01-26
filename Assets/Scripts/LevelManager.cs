00499e898fc1e1cf7b120e7e118b7f4bea4b0b5e
Les Boyd <leslieboyd3@gmail.com>
2025-01-25 19:59:24 -0800

    added Start Menu
    created a title page/start menu scene and added code to detect Escape key press

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,9 +5,20 @@ public class LevelManager : MonoBehaviour
 {
 	public string sceneToLoad = "SampleScene";
 
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            SceneManager.LoadScene("StartMenu");
+        }
+    }
 
-	public void LoadGame ()
+    public void LoadGame ()
 	{
 		SceneManager.LoadScene(sceneToLoad);
 	}
+    public void QuitGame()
+    {
+       Application.Quit();
+    }
 }
